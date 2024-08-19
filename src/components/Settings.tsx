957103120e0209a406be997dc85ea69e6b0b2cc3
mehmetraif12 <mehmetraif33@gmail.com>
2024-08-19 23:51:30 +0300

    denedim

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -3,7 +3,7 @@ const Settings = () => {
   return (
     <div className="h-[100px] bg-yellow-400 m-4 p-4 rounded-md">
         <h1>
-          WHY AREN'T YOU SHOW ME MOTHERFUCKER?!?!
+          
         </h1>
     </div>
   )
